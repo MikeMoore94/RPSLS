37b39f971551557d1346b93ba660284f687b9816
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-10-08 12:54:28 -0400

    made the player class with one method in it

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+
+    def __init__(self):
+        self.name = ''
+        self.gestures = ["rock" , "paper", "scissors" , "lizar", "spock"]
+        self.score = 0 
+        self.chosen_gestures = ''
